ffae65a41125816ae18ae61d0bb31965dddd1d5c
Youssef Tabyaoui <youssef.tabyaoui@telecom-bretagne.eu>
2018-06-22 11:46:35 +0200

    history and bookmark working

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -8,9 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'My first app';
   player: YT.Player;
-  private id: string = 'qDuKsiwS5xw';
+  id: string = 'qDuKsiwS5xw';
   values='abs';
-  history: string[]=['yo'];
+  history: string[]=["https://www.youtube.com/v/qDuKsiwS5xw?version=3"];
+  bookmarks:string[]=[];
+  currentUrl="https://www.youtube.com/v/qDuKsiwS5xw?version=3";
 
   savePlayer(player) {
     this.player = player;
@@ -22,7 +24,26 @@ export class AppComponent {
   onEnter(value:string){   
     
     this.player.loadVideoByUrl(value);
-    this.history.push(value)}
+    this.history.push(value);
+    this.currentUrl=value;}
+  hiClicked(i){
+ 
+    this.player.loadVideoByUrl(this.history[i]);
+    this.history.push(this.history[i]);
+    this.currentUrl=this.history[i];}
+  bClicked(j){
+ 
+    this.player.loadVideoByUrl(this.bookmarks[j]);
+    this.history.push(this.bookmarks[j]);
+    this.currentUrl=this.bookmarks[j];}
+  bookmark(){
+    this.bookmarks.push(this.currentUrl);
+    }
+  
+    
+
+
+
   
                         
 }
